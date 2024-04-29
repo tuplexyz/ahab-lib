467563a224c25b8a9255c90b861a91ea1d9cf6e2
Colby Ford <colbytylerford@gmail.com>
2024-04-29 17:25:26 -0400

    Update cli.py

--- ahab/cli.py
+++ ahab/cli.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-from job_command import JobCommand
+from ahab.job_command import JobCommand
 from cleo.application import Application
 
-cli = Application()
-cli.add(JobCommand())
+def cli():
+    cli = Application()
+    cli.add(JobCommand())
+    cli.run()
 
 if __name__ == '__main__':
-    cli.run()
+    cli()
